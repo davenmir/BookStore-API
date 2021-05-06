ed444088ef82672e6bcc4a39bbfb3712fb272bf1
Michael Davenport <davenmi95@gmail.com>
2021-05-06 09:01:14 -0500

    init

---
+++ BookStore-API/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookStore_API.Controllers
+{
+    /// <summary>
+    /// This is a test API controller
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        /// <summary>
+        /// Get values
+        /// </summary>
+        /// <returns></returns>
+        // GET: api/<HomeController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        /// <summary>
+        /// Get a Value
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        // GET api/<HomeController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        /// <summary>
+        /// Add a new value
+        /// </summary>
+        /// <param name="value"></param>
+        // POST api/<HomeController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+        /// <summary>
+        /// Modify a value 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="value"></param>
+        // PUT api/<HomeController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+        /// <summary>
+        /// Delete a Value
+        /// </summary>
+        /// <param name="id"></param>
+        // DELETE api/<HomeController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
